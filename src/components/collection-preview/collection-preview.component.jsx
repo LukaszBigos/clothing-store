01285f0d0868a6d2a3542845ef8042a02beaac5a
Lukasz Bigos <lukaszbi@herbalife.com>
2021-09-23 08:08:51 +0200

    collection item and preview components

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 
 import "./collection-preview.styles.scss";
+import CollectionItem from "../collection-item/collection-item.component";
 
-const CollectionPreview = ({ items, title }) => {
+const CollectionPreview = ({ title, items }) => {
   return (
     <div className='collection-preview'>
       <h1 className='title'>{title.toUpperCase()}</h1>
@@ -10,7 +11,7 @@ const CollectionPreview = ({ items, title }) => {
         {items
           .filter((item, idx) => idx < 4)
           .map(item => (
-            <div key={item.id}>{item.name}</div>
+            <CollectionItem key={item.id} item={item} />
           ))}
       </div>
     </div>
